c7b41f10163a83292104404fc0b83a615112196d
Thomas Linkin <trlinkin@gmail.com>
2013-05-10 10:46:45 -0400

    Refactor to properly resolve resource from catalog
    Before this commit, this parameter was failing to handle the resource
    reference correctly. I originally assume I would be receiving a
    Puppet::Type::File but instead I was receiving a Puppet::Resource. Due
    to parse time complications, the reference may not always resolve before
    the catalog is done compiling. It should always resolve afterwards.
    After further research and help from @adrienthebo I was able to
    determine that it is safe to not resolve the resource until catalog
    application time. This however does not solve the problem of making sure
    the resource is in a catalog before sending to an agent, however, the
    relationship metaparameters help with that. Eventually, I'd like to hook
    into the 'finish' function called on every type. For now, this works
    just fine. Also, this parameter does not create any resource
    relationships, and never should. That being the case, one should always
    still use the relationship parameters whenever they need a file to exist
    before modifying it.
    
    This commit adds an 'unmunge' function and now works in Puppet
    manifests.

--- lib/puppetx/parameter/file.rb
+++ lib/puppetx/parameter/file.rb
@@ -18,19 +18,24 @@ class PuppetX::Parameter::File < Puppet::Parameter
   def unsafe_validate(value)
     fail("#{name} does not accept an array as input") if value.is_a? Array
 
-    if value.is_a? Puppet::Type::File and !accept_file_with_content?
-      fail("#{value} is managing content, #{name} will not overwrite") if value.to_hash[:content] or value.to_hash[:source]
+    if value.is_a? String
+      fail("#{name} must be a fully qualified path") unless absolute_path?(value)
+    elsif not value.resource_type == Puppet::Type.type(:file)
+      fail("#{name} only accepts strings and File[] references")
     end
-
-    fail("#{name} must be a fully qualified path" ) unless absolute_path?(munge(value))
     value
   end
 
-  # Incase someone Overrides `unsafe_validate` this is a second chance to fight off arrays
-  def unsafe_munge(value)
-    fail("{name} does not accept an array as input") if value.is_a? Array
-    if value.is_a? Puppet::Type::File
-      return value.to_hash[:path]
+  def unmunge(value)
+    if @ref.nil? and value.is_a? Puppet::Resource
+      @ref = resource.catalog.resource(value.to_ref)
+      if @ref and !accept_file_with_content?
+        fail("#{value} is managing content, #{name} will not overwrite") if @ref[:content] or @ref[:source]
+      end
+    end
+
+    if @ref
+      return @ref[:path]
     end
     value
   end
